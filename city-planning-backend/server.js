1c9812e307af218f8c88bbd4e5a1d72d2fffb15f
Shrey0610 <shreyshah241@gmail.com>
2024-11-09 11:45:34 +0530

    started

---
+++ city-planning-backend/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+    })
+
+
+app.get('/api', (req, res) => {
+    res.json({"users":['user1', 'user2', 'user3', 'user4']});
+    })
+
+
+app.listen(PORT, () => {
+    console.log(`Server started at ${PORT}`);
+    })
