3ef2894d335b1b16fe3aaa6665c7d6d222a44c87
Shrey0610 <shreyshah241@gmail.com>
2024-11-09 11:54:28 +0530

    started

--- city-planning-backend/server.js
+++ city-planning-backend/server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 5001;
 app.get('/', (req, res) => {
     res.send('Hello World');
     })
-
+ 
 
 app.get('/api', (req, res) => {
     res.json({"users":['user1', 'user2', 'user3', 'user4']});
